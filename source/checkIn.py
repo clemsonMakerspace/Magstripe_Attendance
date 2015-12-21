306623c834c55b626f4873e31ea04913c4824459
choroid <owen@owenap.com>
2015-12-20 19:41:39 -0500

    Stage all of source files for Eclipse

---
+++ source/checkIn.py
@@ -0,0 +1,46 @@
+import sys
+from UIUtil import UI
+from TextUtil import TextUI
+import constants as c
+import DBUtil
+
+
+def main(args):
+    print("Clemson University Makerspace Attendance Tracker Version" , c.VERSION)
+    # Init textMode
+    textMode = 0
+   # Process the arguments
+    if len(args) > 1:
+        arg = args[1].lower()
+        if arg == "--help":
+            showHelp()
+            sys.exit(0)
+        elif arg == "--version":
+            showVersion()
+            sys.exit(0)
+        elif arg == "--nogui":
+            textMode = 1
+        else:
+            print("Invalid argument:", args[1])
+
+   # Start the program into either textmode or GUI mode
+    if textMode == 0:
+       global app
+       app = UI(args)
+       app.exec_()
+    else:
+        TextUI().start()
+
+   # Exit normally
+    sys.exit(0)
+
+
+def showHelp():
+    print("Supress GUI:\t--nogui\nShow Help:\t--help\nShow Version:\t--version")
+
+def showVersion():
+   print("Version", c.VERSION)
+
+
+if __name__ == '__main__':
+   main(sys.argv)
