e87de2b272f527889ae4c336e5363509e431cd6f
choroid <owen@owenap.com>
2015-12-21 23:23:32 -0500

    Fix whitespacing

--- source/checkIn.py
+++ source/checkIn.py
@@ -29,7 +29,7 @@ def main(args):
     print(c.GROUP_NAME, "Attendance Tracker Version", c.VERSION)
     # Init textMode
     textMode = 0
-   # Process the arguments
+    # Process the arguments
     if len(args) > 1:
         arg = args[1].lower()
         if arg == "--help":
@@ -43,15 +43,15 @@ def main(args):
         else:
             print("Invalid argument:", args[1])
 
-   # Start the program into either textmode or GUI mode
+    # Start the program into either textmode or GUI mode
     if textMode == 0:
-       global app
-       app = UI(args)
-       app.exec_()
+        global app
+        app = UI(args)
+        app.exec_()
     else:
         TextUI().start()
 
-   # Exit normally
+    # Exit normally
     sys.exit(0)
 
 
@@ -59,8 +59,8 @@ def showHelp():
     print("Supress GUI:\t--nogui\nShow Help:\t--help\nShow Version:\t--version")
 
 def showVersion():
-   print("Version", c.VERSION)
+    print("Version", c.VERSION)
 
 
 if __name__ == '__main__':
-   main(sys.argv)
+    main(sys.argv)
