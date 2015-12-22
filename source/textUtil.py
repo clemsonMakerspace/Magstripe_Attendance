06564270809d1499e0ea7b68b9d18e8d9f94e2e2
choroid <owen@owenap.com>
2015-12-22 01:32:19 -0500

    Remove copyright claim until a later date

--- source/textUtil.py
+++ source/textUtil.py
@@ -1,7 +1,6 @@
 #===============================================================================
 #    Magstripe Attendance Database System
 #===============================================================================
-#    Copyright (C) 2015 Owen Phillips
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
