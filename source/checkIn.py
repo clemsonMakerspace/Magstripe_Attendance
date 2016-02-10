307c1f29d58d42790ce9a1b62603972f23f8fe3e
choroid <owen@owenap.com>
2016-02-10 15:08:46 -0500

    Fix shebang - program now works as ./checkIn.py

--- source/checkIn.py
+++ source/checkIn.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #===============================================================================
 #    Magstripe Attendance Database System
 #===============================================================================
@@ -15,7 +17,7 @@
 #    You should have received a copy of the GNU Lesser General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
-#!/usr/bin/env python
+
 import sys
 from ui import UI
 from textUtil import TextUI
